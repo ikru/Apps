61b1d2feaba7967254dcfa94a92161c49944da22
ivan <ikru@inbox.ru>
2021-05-05 00:20:15 -0400

    try npm rebuild node-sass@4.14.1 -no-bin-links

--- spfx-and-azfunc/scripts/spfx-npm-install.sh
+++ spfx-and-azfunc/scripts/spfx-npm-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose exec "spfx-debug" bash -c "npm install --ignore-optional"
+docker-compose exec "spfx-debug" bash -c "npm rebuild node-sass@4.14.1 -no-bin-links && npm install --ignore-optional"
